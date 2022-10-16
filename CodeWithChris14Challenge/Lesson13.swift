7fbcb67d5b509fc940ce9d90e92f61b58fe61926
Marcin Jędrzejak <53284922+maartinj@users.noreply.github.com>
2022-10-17 00:57:57 +0200

    Completed lesson 13

--- CodeWithChris14Challenge/Lesson13.swift
+++ CodeWithChris14Challenge/Lesson13.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct Lesson13: View {
+    @State private var appleCard = "image1"
+    @State private var cherryCard = "image2"
+    @State private var starCard = "image3"
+    @State private var creditsScore = 1000
     var body: some View {
         VStack {
             Spacer()
@@ -18,17 +22,55 @@ struct Lesson13: View {
             
             Spacer()
             
-            Text("Credits: 1000")
+            Text("Credits: \(creditsScore)")
                 .font(.callout)
             
             Spacer()
             
-            Image("apple")
+            HStack {
+                Image(String(appleCard))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .animation(.default, value: appleCard)
+                Image(String(cherryCard))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .animation(.default, value: cherryCard)
+                Image(String(starCard))
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .animation(.default, value: starCard)
+            }
+            .padding()
             
             Spacer()
             
             Button("Spin") {
                 
+                // Generate a random number between 1 and 3
+                let appleNumber = Int.random(in: 1...3)
+                let cherryNumber = Int.random(in: 1...3)
+                let starNumber = Int.random(in: 1...3)
+                
+                // Update the images
+                appleCard = "image" + String(appleNumber)
+                cherryCard = "image" + String(cherryNumber)
+                starCard = "image" + String(starNumber)
+                
+                // Update the credits
+                if appleCard == cherryCard {
+                    if appleCard == starCard {
+                        if cherryCard == starCard {
+                            creditsScore += 30
+                        } else {
+                            creditsScore -= 5
+                        }
+                    } else {
+                        creditsScore -= 5
+                    }
+                } else {
+                    creditsScore -= 5
+                }
             }
             .frame(width: 120, height: 35)
             .background(.red)
